2a13f9cae9d5a84dc3a91e79087e437d5b66001b
gurbirbains <baingurb@shernet.sheridancollege.ca>
2024-05-26 21:58:44 -0400

    added images for each campus

--- src/components/Home.js
+++ src/components/Home.js
@@ -3,9 +3,9 @@ import { Link } from 'react-router-dom';
 
 function Home() {
   const locations = [
-    { name: 'Davis', image: 'https://via.placeholder.com/200x150?text=Davis', link: '/campus/Davis' },
-    { name: 'HMC', image: 'https://via.placeholder.com/200x150?text=HMC', link: '/campus/HMC' },
-    { name: 'Trafalgar', image: 'https://via.placeholder.com/200x150?text=Trafalgar', link: '/campus/Trafalgar' },
+    { name: 'Davis Campus', image: 'https://media-www.sheridancollege.ca/-/media/project/sheridan/shared/images/programs/program-pages/program-index/campus/davis_campus_entrance.png?rev=0aa7dcf56e9548b5a7b16a27ef73dbec&w=1024&hash=EB01BB972427B390D312350D4C71BDDA', link: '/campus/Davis' },
+    { name: 'Hazel Mccallion Campus', image: 'https://lh3.googleusercontent.com/p/AF1QipMYJGHBJXKiNcV1jVNecd0Fvkru6lUNcbiA6XRD=s1360-w1360-h1020', link: '/campus/HMC' },
+    { name: 'Trafalgar Campus', image: 'https://media-www.sheridancollege.ca/-/media/project/sheridan/shared/images/programs/program-pages/program-index/campus/trafalgar-road-campus-sheridan-college.jpg?rev=21f870310fa244b6aebe48f9b4a5a4c6&w=1024&hash=CC46959813E3F95E06C2FBEBD75F71C3', link: '/campus/Trafalgar' },
   ];
 
   return (
