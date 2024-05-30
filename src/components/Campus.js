b730736785674841319b3c411f2c610070054da7
gurbirbains <baingurb@shernet.sheridancollege.ca>
2024-05-30 14:17:40 -0400

    fixed

--- src/components/Campus.js
+++ src/components/Campus.js
@@ -1,12 +1,10 @@
-import React, { useState, useEffect } from 'react';
+import React from 'react';
 import { Link, useParams } from 'react-router-dom';
 import './Campus.css';
 
 function Campus() {
   const { campusName } = useParams();
-  const [items, setItems] = useState([]);
 
-<<<<<<< HEAD
   const items = [
     { label: 'Chargers', count: 20, type: 'chargers' },
     { label: 'Bottles', count: 5, type: 'bottles' },
@@ -15,23 +13,6 @@ function Campus() {
     { label: 'Hats', count: 4, type: 'hats' },
     { label: 'Gloves', count: 6, type: 'gloves' },
   ];
-=======
-  useEffect(() => {
-    fetch(`https://localhost:7224/api/items/location/${campusName}`)
-      .then(response => {
-        if (!response.ok) {
-          throw new Error('Network response was not ok');
-        }
-        return response.json();
-      })
-      .then(data => setItems(data))
-      .catch(error => console.error('Failed to fetch items', error));
-  }, [campusName]);
-
-  if (!items.length) {
-    return <div>No items found for this campus</div>;
-  }
->>>>>>> 15c1fc4f98cb77dc111f239bcaf534b8b5924d61
 
   return (
     <div className="campus-container">
@@ -40,14 +21,10 @@ function Campus() {
       <div className="items-container">
         {items.map((item, index) => (
           <div className="item" key={index}>
-            <div className="item-placeholder">{item.category[0]}</div>
-            <div>{item.category}</div>
+            <div className="item-placeholder">{item.label[0]}</div>
+            <div>{item.label}</div>
             <div>{item.count}</div>
-<<<<<<< HEAD
             <Link to={`/campus/${campusName}/${item.type}`}>Details</Link>
-=======
-            <Link to={`/campus/${campusName}/${item.category}`}>Details</Link>
->>>>>>> 15c1fc4f98cb77dc111f239bcaf534b8b5924d61
           </div>
         ))}
       </div>
